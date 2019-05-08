d8570f009e8b676dc5ec2c58585359ca72477ef0
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-08 10:04:48 -0600

    Add logic to get appropriate API endpoints
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -4,8 +4,28 @@ class GithubFacade
     @token = user[:github_token]
   end
 
+  def repos(limit = 5)
+    api_data(:repos)[0..(limit-1)].map do |repo|
+      GithubRepo.new(repo)
+    end
+  end
+
+  def followers
+    api_data(:followers).map do |follower|
+      GithubUser.new(follower)
+    end
+  end
+
   private
 
+  def api_data(endpoint)
+    case endpoint
+    when :repos
+      @_repo_data ||= service.get_repos
+    when :followers
+      @_follower_data ||= service.get_followers
+    end
+  end
 
   def service
     @_service ||= GithubService.new(@token)
