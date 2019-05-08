443f39d21b0779343692e1f132c1d84af1b8e303
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-08 10:06:54 -0600

    Add model test for GithubUser
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

---
+++ spec/models/github_user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe GithubUser do
+  it "has attributes" do
+    attributes = {
+          login: "Test User",
+          html_url: "https://test_link.com"
+          }
+
+    test_user = GithubUser.new(attributes)
+
+    expect(test_user.handle).to eq('Test User')
+    expect(test_user.profile_url).to eq('https://test_link.com')
+  end
+end
