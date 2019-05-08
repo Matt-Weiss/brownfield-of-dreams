f305540f1ad5fcbb0a39b5b97cdf1653ce9a9b28
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-08 11:03:19 -0600

    Remove get_ from reader method names
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -5,29 +5,29 @@ class GithubService
     @token = token
   end
 
-  def get_repos
+  def repos
     get_json('user/repos')
   end
 
-  def get_followers
+  def followers
     get_json('user/followers')
   end
 
-  def get_following
+  def following
     get_json('user/following')
   end
 
   private
 
-    def get_json(url)
-      response = conn.get(url)
-      data = JSON.parse(response.body, symbolize_names: true)
-    end
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
-    def conn
-      Faraday.new("https://api.github.com/") do |f|
-        f.headers["Authorization"] = "token #{@token}"
-        f.adapter Faraday.default_adapter
-      end
+  def conn
+    Faraday.new('https://api.github.com/') do |f|
+      f.headers['Authorization'] = "token #{@token}"
+      f.adapter Faraday.default_adapter
     end
+  end
 end
