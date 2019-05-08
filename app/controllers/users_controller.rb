ed5b7bbeae84446fb0b5c42ff7d6e651f667f773
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-08 10:05:48 -0600

    Adjust UsersController to use new facade
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 class UsersController < ApplicationController
   def show
     render locals: {
-      repo_facade: RepoFacade.new(current_user)
+      github_facade: GithubFacade.new(current_user)
     }
   end
 
