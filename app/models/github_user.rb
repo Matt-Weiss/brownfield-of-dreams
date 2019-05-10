995379ac7ba38f6ca8343fb125c115ae6f32d6f6
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-09 22:16:39 -0600

    Add github_id to GithubUser model

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -2,10 +2,12 @@
 
 class GithubUser
   attr_reader :handle,
-              :profile_url
+              :profile_url,
+              :github_id
 
   def initialize(user_hash)
     @handle = user_hash[:login]
     @profile_url = user_hash[:html_url]
+    @github_id = user_hash[:id]
   end
 end
