2bad852f22eaeb6966ec94d7dcc51716e428a8ec
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-11 19:35:22 -0600

    Add flash message on error adding friend

--- app/controllers/users/friendships_controller.rb
+++ app/controllers/users/friendships_controller.rb
@@ -1,6 +1,10 @@
 class Users::FriendshipsController < ApplicationController
   def create
     service = FriendshipService.new(current_user.github_id)
-    service.add_friend(params[:friend_id])
+    status, response = service.add_friend(params[:friend_id])
+    if status != 200
+      flash[:error] = "Unable to befriend that user."
+    end
+    redirect_to dashboard_path
   end
 end
