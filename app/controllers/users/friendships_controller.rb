ba3f7a426711891e68685269ff3aaba74bb9e906
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-12 13:54:39 -0600

    Block adding a friend without a local acct

--- app/controllers/users/friendships_controller.rb
+++ app/controllers/users/friendships_controller.rb
@@ -1,8 +1,12 @@
 class Users::FriendshipsController < ApplicationController
   def create
-    service = FriendshipService.new(current_user.github_id)
-    status, response = service.add_friend(params[:friend_id])
-    if status != 200
+    if User.find_by(github_id: params[:friend_id])
+      service = FriendshipService.new(current_user.github_id)
+      status, response = service.add_friend(params[:friend_id])
+      if status != 200
+        flash[:error] = "Unable to befriend that user."
+      end
+    else
       flash[:error] = "Unable to befriend that user."
     end
     redirect_to dashboard_path
