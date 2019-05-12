d22f1fb2735f90d6d89fb8fb9f730e731b4b2552
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-11 19:35:22 -0600

    Change tests to better represent expected behavior

--- spec/features/user/user_can_add_followers_as_friends_spec.rb
+++ spec/features/user/user_can_add_followers_as_friends_spec.rb
@@ -4,23 +4,34 @@ describe 'As a logged in user' do
  context 'When I visit /dashboard' do
     context 'Then I should be able to add friends' do
       before :each do
-        response = File.new('./spec/data/user_a_followers.txt')
-        stub_request(:get, 'https://api.github.com/user/followers').to_return(response)
+        followers = File.new('./spec/data/user_b_followers.txt')
+        stub_request(:get, 'https://api.github.com/user/followers').to_return(followers)
 
-        @user = User.create!(email: "User@example.com",
-                        first_name: "Jon",
-                         last_name: "Peterson",
-                          password: "password",
-                              role: 0,
-                      github_token: ENV["GITHUB_API_TOKEN_A"]
-                            )
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+        following = File.new('./spec/data/user_b_following.txt')
+        stub_request(:get, 'https://api.github.com/user/following').to_return(following)
+
+        create_friendship = File.new('./spec/data/friendship_request_successful.txt')
+        stub_request(:post, 'https://young-mountain-25786.herokuapp.com/api/v1/friendships').to_return(create_friendship)
+
+        get_friendship = File.new('./spec/data/empty_friendships_list.txt')
+        stub_request(:get, 'https://young-mountain-25786.herokuapp.com/api/v1/friendships/45211960').to_return(get_friendship)
+
+        @user1 = create(:user,
+                github_token: ENV["GITHUB_API_TOKEN_B"],
+                   github_id: 45211960
+                      )
+
+        @user2 = create(:user,
+                github_token: ENV["GITHUB_API_TOKEN_A"],
+                   github_id: 3322920
+                      )
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
       end
       it 'I see a button to add followers if they have an account here' do
         visit dashboard_path
 
         within ".followers" do
-          expect(page).to have_css("a[href='https://github.com/Matt-Weiss'] ~ form[action='/users/friendships']")
+          expect(page).to have_css("a[href='https://github.com/joequincy'] ~ form[action='/users/friendships']")
         end
       end
 
@@ -28,13 +39,18 @@ describe 'As a logged in user' do
         visit dashboard_path
 
         within ".friends" do
-          expect(page).to_not have_content("Matt-Weiss")
+          expect(page).to_not have_content(@user2.first_name)
         end
 
-        click_link "Add as Friend"
+        get_friendship = File.new('./spec/data/friendships_list.txt')
+        stub_request(:get, 'https://young-mountain-25786.herokuapp.com/api/v1/friendships/45211960').to_return(get_friendship)
+
+        within ".followers" do
+          click_button "Add as Friend"
+        end
 
         within ".friends" do
-          expect(page).to have_content("Matt-Weiss")
+          expect(page).to have_content(@user2.first_name)
         end
       end
 
