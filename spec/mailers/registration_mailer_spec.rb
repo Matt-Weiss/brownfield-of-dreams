496100ad76ee3ee47e5f73ae0365be4d664fa069
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-15 19:41:15 -0600

    Add mailer test

--- spec/mailers/registration_mailer_spec.rb
+++ spec/mailers/registration_mailer_spec.rb
@@ -1,5 +1,13 @@
 require "rails_helper"
 
 RSpec.describe RegistrationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "sends an activation email for the user" do
+    user = create(:user)
+    email = RegistrationMailer.confirm(user)
+
+    expect(email.subject).to eq('Activate your account')
+    expect(email.to.first).to eq(user.email)
+    expect(email.from.first).to eq('noreply@serene-gorge-80745.herokuapp.com')
+    expect(email.body.encoded).to match("/users/#{user.id}/activate")
+  end
 end
