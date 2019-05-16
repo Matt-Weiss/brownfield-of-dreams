ead4942695aff561e0b6657352fd22e4c6102e67
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-16 15:26:55 -0600

    Add test to ensure API call reorders tutorials

--- spec/requests/admin/api/v1/tutorials_request_spec.rb
+++ spec/requests/admin/api/v1/tutorials_request_spec.rb
@@ -7,4 +7,27 @@ describe 'Admin Tutorials API' do
     tutorial1 = create(:tutorial)
     expect {put "/admin/api/v1/tutorial_sequencer/#{tutorial1.id}"}.to raise_error(ActionController::RoutingError)
   end
+
+  it 'reorders the tutorial videos' do
+    tutorial1 = create(:tutorial)
+    tutorial2 = create(:tutorial)
+
+    video1 = create(:video, tutorial_id: tutorial1.id)
+    video2 = create(:video, tutorial_id: tutorial1.id)
+    video3 = create(:video, tutorial_id: tutorial2.id)
+    video4 = create(:video, tutorial_id: tutorial2.id)
+
+    admin = create(:admin)
+    allow_any_instance_of(Admin::Api::V1::BaseController).to receive(:current_user).and_return(admin)
+
+    sorted_ids = [video2.id,video1.id].to_json
+    put "/admin/api/v1/tutorial_sequencer/#{tutorial1.id}",
+        params: sorted_ids,
+        env: {
+          'HTTP_ACCEPT' => 'application/json',
+          'CONTENT_TYPE' => 'application/json'
+        }
+    expect(Video.find(video2.id).position).to eq(1)
+    expect(Video.find(video1.id).position).to eq(2)
+  end
 end
