517949a7ee9412ba9fd32acbcba91aab4e83fbcf
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-15 19:40:45 -0600

    Generate mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'noreply@serene-gorge-80745.herokuapp.com'
   layout 'mailer'
 end
