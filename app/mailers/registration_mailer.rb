cb02d257f613f147071c00c2e484e0c7f77fdcd0
Matt Weiss <weiss.matt@outlook.com>
2019-05-17 16:15:07 -0600

    Adds invite method to mailer
    Co-authored-by: Jon Peterson <3322920+joequincy@users.noreply.github.com>

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -3,4 +3,10 @@ class RegistrationMailer < ApplicationMailer
     @user = user
     mail(to: user.email, subject: "Activate your account")
   end
+
+  def invite(email_address, invitee_name, inviter_name)
+    @invitee_name = invitee_name
+    @inviter_name = inviter_name
+    mail(to: email_address, subject: "Invitation to Join Turing Tutorials")
+  end
 end
