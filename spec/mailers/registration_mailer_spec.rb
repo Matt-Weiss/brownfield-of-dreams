a03d1f679801110eb957c3c0de10b163dfea0994
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-17 16:55:56 -0600

    Add mailer test for invite
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- spec/mailers/registration_mailer_spec.rb
+++ spec/mailers/registration_mailer_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe RegistrationMailer, type: :mailer do
     expect(email.from.first).to eq('noreply@serene-gorge-80745.herokuapp.com')
     expect(email.body.encoded).to match("/users/#{user.id}/activate")
   end
+
+  it "sends an invite email to another user" do
+    inviter = {login: "inviter"}
+    invitee = {login: "invitee", email: "test@test.com"}
+    email = RegistrationMailer.invite(inviter, invitee)
+
+    expect(email.subject).to eq('Invitation to Join Turing Tutorials')
+    expect(email.to.first).to eq(invitee[:email])
+    expect(email.from.first).to eq('noreply@serene-gorge-80745.herokuapp.com')
+    expect(email.body.encoded).to match("/register")
+  end
 end
