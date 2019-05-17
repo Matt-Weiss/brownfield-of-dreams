856bc27aeb881a2c5389240a7598d29aa9dd4ace
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-17 17:06:28 -0600

    Rubocop cleanup
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
 class InvitesController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     invite = Invite.new(params[:login], current_user)
     if invite.send
-      flash[:message] = "Successfully sent invite!"
+      flash[:message] = 'Successfully sent invite!'
     else
-      flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+      flash[:error] = 'The Github user you selected doesn\'t have an email
+                       address associated with their account.'
     end
     redirect_to dashboard_path
   end
-
 end
