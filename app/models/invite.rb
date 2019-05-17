549c92eedc95699f4f5dfebf96437890edca5993
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-17 16:57:39 -0600

    Move send action to separate method
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,16 +1,17 @@
 class Invite
 
   def initialize(login, current_user)
-    service ||= GithubService.new(current_user.github_token)
-    user_github_name = service.name
-    invited_user = service.fetch_invited_user_email(login)
-    email = invited_user[:email]
-    invited_username = invited_user[:login]
+    service = GithubService.new(current_user.github_token)
+    @inviter = service.user
+    @invitee = service.user(login)
+  end
 
-    if email
-      RegistrationMailer.invite(email, invited_username, user_github_name)
+  def send
+    if @invitee[:email]
+      RegistrationMailer.invite(@inviter, @invitee)
+      true
     else
-      #shit don't work
+      false
     end
   end
 end
