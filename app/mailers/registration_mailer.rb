2e47d3fd8d1c5e222b1f73c6ffdff495da9247c2
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-17 16:59:35 -0600

    Change mailer to accept user hashes
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -4,9 +4,9 @@ class RegistrationMailer < ApplicationMailer
     mail(to: user.email, subject: "Activate your account")
   end
 
-  def invite(email_address, invitee_name, inviter_name)
-    @invitee_name = invitee_name
-    @inviter_name = inviter_name
-    mail(to: email_address, subject: "Invitation to Join Turing Tutorials")
+  def invite(inviter, invitee)
+    @invitee = invitee
+    @inviter = inviter
+    mail(to: @invitee[:email], subject: "Invitation to Join Turing Tutorials")
   end
 end
