ca7b26a24584658a3b8dfff188730f3809d64847
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-17 17:07:16 -0600

    Rubocop cleanup
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -3,12 +3,12 @@
 class RegistrationMailer < ApplicationMailer
   def confirm(user)
     @user = user
-    mail(to: user.email, subject: "Activate your account")
+    mail(to: user.email, subject: 'Activate your account')
   end
 
   def invite(inviter, invitee)
     @invitee = invitee
     @inviter = inviter
-    mail(to: @invitee[:email], subject: "Invitation to Join Turing Tutorials")
+    mail(to: @invitee[:email], subject: 'Invitation to Join Turing Tutorials')
   end
 end
