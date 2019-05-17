006b853047af1df019ed273ad32a7fb521f936f7
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-17 16:57:07 -0600

    Add flash message to show outcome of sending email
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -4,6 +4,11 @@ class InvitesController < ApplicationController
 
   def create
     invite = Invite.new(params[:login], current_user)
+    if invite.send
+      flash[:message] = "Successfully sent invite!"
+    else
+      flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+    end
     redirect_to dashboard_path
   end
 
